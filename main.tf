1d51fe3232ae04000d753962d1a875b88e0dc19b
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2026-02-15 23:48:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,18 @@ provider "aws" {
     region = "eu-west-1"
 }
 
+#VCP
+
+resource "aws_vpc" "demo_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
 
 resource "aws_s3_bucket" "demo"  {
     bucket = "joshua-terraform-demo-bucket-001"
