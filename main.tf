998135cf121d1dde18677f0f07b51f5cb09633c5
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2026-03-12 23:59:33 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
-
 
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
-#VPC
-
+# VPC
 resource "aws_vpc" "demo_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -24,7 +22,18 @@ resource "aws_vpc" "demo_vpc" {
   }
 }
 
+# Subnet inside the VPC
+resource "aws_subnet" "demo_subnet" {
+  vpc_id            = aws_vpc.demo_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
 
-resource "aws_s3_bucket" "demo"  {
-    bucket = "joshua-terraform-demo-bucket-001"
+  tags = {
+    Name = "demo-subnet"
+  }
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "demo" {
+  bucket = "joshua-terraform-demo-bucket-001"
 }
